d3d0f7d0ee4d6e9775e9a1efe0767197bbf1fefa
ryuichi.nakano <blowinblues582@gmail.com>
2020-07-11 23:33:06 +0900

    コントローラーの変更

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -9,6 +9,17 @@ class RecordsController < ApplicationController
     @record = Record.new
   end
 
+  def create
+    record = Record.new(record_params)
+    record.save
+    redirect_to records_url, notice: "コーヒー「#(record.name)」を登録いたしました。"
   def edit
+  end
+end
+
+private
+
+  def record_params
+    params.require(:record).permit(:name, :description)
   end
 end
